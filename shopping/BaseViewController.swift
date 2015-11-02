06f803f4a4893edc6e1d993805495f945cc89f8f
liguo.yu <597246676@qq.com>
2015-11-02 18:40:15 +0800

    百度糯米项目

---
+++ shopping/BaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  shopping
+//
+//  Created by yek on 15/11/2.
+//  Copyright © 2015年 yek. All rights reserved.
+//
+
+import UIKit
+
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
